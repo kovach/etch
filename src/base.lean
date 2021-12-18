074e1253670ecc2beb2b5cf8ab92af8c604bd528
Scott Kovach <dskovach@gmail.com>
2021-12-17 18:37:04 -0700

    use `function.End` in `iter` definition

--- src/base.lean
+++ src/base.lean
@@ -8,28 +8,10 @@ import logic.relation
 universes u v
 variables (α β : Type*)
 
--- todo, already exists as function.End.smul_def
-instance comp_monoid (ν : Type*) : monoid (ν → ν) :=
-{ mul := λ a b, a ∘ b , mul_assoc := λ a b c, rfl
-, one := λ a, a , one_mul := λ a, rfl , mul_one := λ a, rfl }
-
-instance endo_action : mul_action (α → α) α :=
-{ smul := λ f a, f a
-, one_smul := λ a, begin unfold has_one.one mul_one_class.one monoid.one, end
-, mul_smul := λ f g x , begin unfold has_mul.mul mul_one_class.mul monoid.mul function.comp, end }
-
--- not used
-instance fish_monoid (m : _) (ν : Type) [monad m] [is_lawful_monad m] : monoid (ν → m ν) :=
-{ mul := λ a b, (a >=> b)
-, mul_assoc := λ a b c, begin unfold has_mul.mul, rw fish_assoc, end --needed for rev version?
-, one := pure
-, one_mul := fish_pipe
-, mul_one := fish_pure
-}
-
+-- use function.End rather than (σ → σ) for the monoid definition and mul_action
 def emit_type (I V : Type) := option (I × option V)
 structure iter (σ I V : Type) [linear_order I] :=
-  (δ : σ → σ)
+  (δ : function.End σ)
   (emit : σ → emit_type I V)
 
 namespace iter
