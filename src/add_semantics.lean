a07d4bc94f19890ee8d581d234f10973b66e4f26
Scott Kovach <dskovach@gmail.com>
2021-12-17 20:19:19 -0700

    tweaks

--- src/add_semantics.lean
+++ src/add_semantics.lean
@@ -16,10 +16,10 @@ variables (s t : σ)
 section semantics
 variable [add_comm_monoid V]
 
-@[simp] theorem terminal_zero {a : iter σ I V} (h : a.terminal t) : a.semantics₁ t = 0 := by simp *
+@[simp] lemma terminal_semantics₁_zero {a : iter σ I V} (h : a.terminal t) : a.semantics₁ t = 0 := by simp *
 
 @[simp]
-theorem semantics_zero {t} {a : iter σ I V} (m : a.monotonic) (h : a.terminal t) (j:ℕ) : a.semantics' t j = 0 := begin
+theorem terminal_zero {t} {a : iter σ I V} (m : a.monotonic) (h : a.terminal t) (j:ℕ) : a.semantics' t j = 0 := begin
 induction j with _ jh generalizing t,
 all_goals {simp *}
 end
@@ -61,20 +61,19 @@ induction n with n hn generalizing s₁ s₂ i j,
 
 obtain (⟨hs,nta,h⟩|⟨hs,ntdi,h⟩|⟨hs,ntb,h⟩) := step_sem_trichotomy a b s₁ s₂,
 
-{ obtain ⟨i', hisucc⟩ := not_terminal_succ nta afin,
-  rw hisucc,
-  simp only [iter.semantics'],
-  simp only [hs,h],
+{ -- a.δ
+  obtain ⟨i', hisucc⟩ := not_terminal_succ nta afin,
   rw hisucc at *,
-  have : i' + j = n,
-  { simpa only [nat.succ_add] using hnij },
-  rw hn (step_progress afin) bfin this,
-  rw add_assoc,
+  simp only [semantics'],
+  rw [hs,h],
+  rw hn (step_progress afin) bfin _,
+  { rw add_assoc },
+  { simp [*, nat.succ_add] at * },
 },
 
 { obtain (⟨ta, tb⟩|⟨nta,ntb⟩) := ntdi,
 
-  { simp [*, semantics_zero, add_iter_monotonic, add_iter_terminal] },
+  { simp only [*, terminal_zero, add_zero, add_iter_monotonic, add_iter_terminal] },
 
   {
     obtain ⟨i', hisucc⟩ := not_terminal_succ nta afin,
@@ -82,25 +81,22 @@ obtain (⟨hs,nta,h⟩|⟨hs,ntdi,h⟩|⟨hs,ntb,h⟩) := step_sem_trichotomy a 
     simp only [hisucc, hjsucc] at *,
     simp only [semantics', hs, h],
     replace afin := step_progress afin,
-    have asem_mono := semantics_mono amono afin (nat.le_succ _),
-    rw asem_mono,
-    have := terminal_by_mono i' i'.succ amono afin (nat.le_succ _),
-    have := hn this (step_progress bfin) (nat.succ.inj hnij),
+    rw semantics_mono amono afin (nat.le_succ _),
+    have afin' := terminal_by_mono i' i'.succ amono afin (nat.le_succ _),
+    have := hn afin' (step_progress bfin) (nat.succ.inj hnij),
     rw this,
     abel,
   },
 },
 
-{ obtain ⟨j', hjsucc⟩ := not_terminal_succ ntb bfin,
-  rw hjsucc,
-  simp only [iter.semantics'],
-  simp only [hs,h],
+{ -- b.δ
+  obtain ⟨j', hjsucc⟩ := not_terminal_succ ntb bfin,
   rw hjsucc at *,
-  have : i + j' = n,
-  { simpa only [nat.add_succ] using hnij },
-  replace bfin := step_progress bfin,
-  rw hn afin bfin this,
-  abel,
+  simp only [iter.semantics'],
+  rw [hs, h],
+  rw hn afin (step_progress bfin) _,
+  { abel },
+  { simp [*, nat.add_succ] at * },
 },
 end
 
