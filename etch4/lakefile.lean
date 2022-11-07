14c6b8de0f36f0fe71c42822de364dd23735d14b
Scott Kovach <dskovach@gmail.com>
2022-11-06 20:48:01 -0800

    remove Aesop from lakefile

--- etch4/lakefile.lean
+++ etch4/lakefile.lean
@@ -9,7 +9,7 @@ lean_exe sep {
   root := `Main
 }
 
-require aesop from git
-  "https://github.com/JLimperg/aesop"
+--require aesop from git
+--  "https://github.com/JLimperg/aesop"
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4/"@"f493ef4c7bbdfc42f14962390c505c097a1650f9"
