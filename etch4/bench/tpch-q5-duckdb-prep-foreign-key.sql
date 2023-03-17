e45ac8fd35a7a303a1bd666ba09daa5fc888314b
Timothy Gu <timothygu99@gmail.com>
2023-03-17 08:45:52 -0700

    Add DuckDB variant that doesn't use foreign keys

--- etch4/bench/tpch-q5-duckdb-prep-foreign-key.sql
+++ etch4/bench/tpch-q5-duckdb-prep-foreign-key.sql
@@ -8,21 +8,21 @@ CREATE TABLE REGION  ( R_REGIONKEY  INTEGER NOT NULL,
                             R_NAME       CHAR(25) NOT NULL,
                             PRIMARY KEY (R_REGIONKEY));
 CREATE TABLE NATION  ( N_NATIONKEY  INTEGER NOT NULL,
-                            N_REGIONKEY  INTEGER NOT NULL,
+                            N_REGIONKEY  INTEGER NOT NULL REFERENCES REGION (R_REGIONKEY),
                             N_NAME       CHAR(25) NOT NULL,
                             PRIMARY KEY (N_NATIONKEY));
 CREATE TABLE SUPPLIER ( S_SUPPKEY     INTEGER NOT NULL,
-                             S_NATIONKEY   INTEGER NOT NULL,
+                             S_NATIONKEY   INTEGER NOT NULL REFERENCES NATION (N_NATIONKEY),
                              PRIMARY KEY (S_SUPPKEY));
 CREATE TABLE CUSTOMER ( C_CUSTKEY     INTEGER NOT NULL,
-                             C_NATIONKEY   INTEGER NOT NULL,
+                             C_NATIONKEY   INTEGER NOT NULL REFERENCES NATION (N_NATIONKEY),
                              PRIMARY KEY (C_CUSTKEY));
 CREATE TABLE ORDERS  ( O_ORDERKEY       INTEGER NOT NULL,
-                           O_CUSTKEY        INTEGER NOT NULL,
+                           O_CUSTKEY        INTEGER NOT NULL REFERENCES CUSTOMER (C_CUSTKEY),
                            O_ORDERDATE      DATE NOT NULL,
                            PRIMARY KEY (O_ORDERKEY));
-CREATE TABLE LINEITEM ( L_ORDERKEY    INTEGER NOT NULL,
-                             L_SUPPKEY     INTEGER NOT NULL,
+CREATE TABLE LINEITEM ( L_ORDERKEY    INTEGER NOT NULL REFERENCES ORDERS (O_ORDERKEY),
+                             L_SUPPKEY     INTEGER NOT NULL REFERENCES SUPPLIER (S_SUPPKEY),
                              L_LINENUMBER  INTEGER NOT NULL,
                              L_EXTENDEDPRICE  DOUBLE NOT NULL, -- actually DECIMAL(15,2), but etch uses double
                              L_DISCOUNT    DOUBLE NOT NULL,
@@ -36,3 +36,4 @@ COPY ORDERS   FROM 'tpch-csv-q5/orders.csv'   (HEADER TRUE, DELIMITER ',');
 COPY LINEITEM FROM 'tpch-csv-q5/lineitem.csv' (HEADER TRUE, DELIMITER ',');
 
 PRAGMA database_size;
+
