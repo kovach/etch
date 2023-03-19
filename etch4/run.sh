49e724a528df1d99bb3bbe2bfd2d9e1dc2164a32
Timothy Gu <timothygu99@gmail.com>
2023-03-19 00:00:48 -0700

    q9 run.sh

--- etch4/run.sh
+++ etch4/run.sh
@@ -19,3 +19,24 @@ for size in x0.01 x0.025 x0.05 x0.1 x0.25 x0.5 x1 x2 x4; do
 	sed -n 's/q5 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q5-etch.txt | awk '{x+=$0}END{print "etch:",x/NR}'
 	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q5-sqlite.txt | awk '{x+=$0}END{print "sqlite:",x/NR}'
 done
+
+for size in x0.01 x0.025 x0.05 x0.1 x0.25 x0.5 x1 x2 x4; do
+	for db in duckdb duckdbforeign etch sqlite; do
+		# warm up
+		# echo run-tpch-$size-q9-$db time -2
+		make run-tpch-$size-q9-$db >/dev/null
+		# echo run-tpch-$size-q9-$db time -1
+		make run-tpch-$size-q9-$db >/dev/null
+
+		rm -f bench-output/run-tpch-$size-q9-$db.txt
+		for i in `seq 5`; do
+			# echo run-tpch-$size-q9-$db time $i
+			make run-tpch-$size-q9-$db >>bench-output/run-tpch-$size-q9-$db.txt
+		done
+	done
+	echo run-tpch-$size-q9
+	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-duckdb.txt | awk '{x+=$0}END{print "duckdb:",x/NR}'
+	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-duckdbforeign.txt | awk '{x+=$0}END{print "duckdbforeign:",x/NR}'
+	sed -n 's/q9 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-etch.txt | awk '{x+=$0}END{print "etch:",x/NR}'
+	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-sqlite.txt | awk '{x+=$0}END{print "sqlite:",x/NR}'
+done
