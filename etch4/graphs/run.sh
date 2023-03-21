bac33d00a44755d35bef18c9727d3267e591238d
Timothy Gu <timothygu@stanford.edu>
2023-03-20 23:54:08 -0700

    add wcoj run.sh

--- etch4/graphs/run.sh
+++ etch4/graphs/run.sh
@@ -43,3 +43,34 @@ for size in x0.01 x0.025 x0.05 x0.1 x0.25 x0.5 x1 x2 x4; do
 	sed -n 's/q9 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-etch.txt | awk '{x+=$0}END{print "etch:",x/NR}'
 	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-tpch-$size-q9-sqlite.txt | awk '{x+=$0}END{print "sqlite:",x/NR}'
 done
+
+# WCOJ
+for size in x1 x3 x10 x30 x100 x300 x1000 x3000 x10000; do
+	make wcoj-csv-$size wcoj-$size.db
+done
+for size in x1 x3 x10 x30 x100 x300 x1000 x3000 x10000; do
+	dbs='duckdb etch sqlite'
+	if [[ $size = x300 ]]; then
+		dbs='duckdb etch'
+	elif [[ $size = x1000 || $size = x3000 || $size = x10000 ]]; then
+		dbs='etch'
+	fi
+
+	for db in $dbs; do
+		# warm up
+		# echo run-wcoj-$size-$db time -2
+		make run-wcoj-$size-$db >/dev/null
+		# echo run-wcoj-$size-$db time -1
+		make run-wcoj-$size-$db >/dev/null
+
+		rm -f bench-output/run-wcoj-$size-$db.txt
+		for i in `seq 5`; do
+			# echo run-wcoj-$size-$db time $i
+			make run-wcoj-$size-$db >>bench-output/run-wcoj-$size-$db.txt
+		done
+	done
+	echo run-wcoj-$size
+	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-wcoj-$size-duckdb.txt | awk '{x+=$0}END{print "duckdb:",x/NR}'
+	sed -n 's/wcojx1000 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-wcoj-$size-etch.txt | awk '{x+=$0}END{print "etch:",x/NR}'
+	sed -n 's/q2 took (s): real \([^ ]*\)s.*/\1/p' <bench-output/run-wcoj-$size-sqlite.txt | awk '{x+=$0}END{print "sqlite:",x/NR}'
+done
