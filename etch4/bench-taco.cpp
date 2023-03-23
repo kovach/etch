c97fac337d601fae67db8953de24a80968d89191
Timothy Gu <timothygu99@gmail.com>
2023-03-23 16:22:58 -0700

    Update cpp

--- etch4/bench-taco.cpp
+++ etch4/bench-taco.cpp
@@ -6,47 +6,47 @@
 #include "operators.h"
 #include "sqlite3.h"
 
-// populated later
-#define MAX_SCALE 100000
-
 int dim = 10000;
 static sqlite3* db;
 int res;
 double threshold = 0.1;
 
+/* clang-format off */
 #include "decls.c"
 #include "taco_kernels.c"
+
 #include "gen_taco.c"
+/* clang-format on */
 
 static int populate_taco(sqlite3* db) {
   char* zErrMsg;
   int rc;
   void* data = NULL;
 
-#define GET_TBL2(out_name, tbl_name, col1, col2)                                      \
+#define GET_TBL2(out_name, tbl_name, col1, col2)                            \
   do {                                                                      \
     rc = sqlite3_exec(                                                      \
         db, "SELECT " #col1 ", " #col2 " FROM " #tbl_name " ORDER BY 1, 2", \
-        gen_taco_##out_name##_callback, (void*)data, &zErrMsg);    \
+        gen_taco_##out_name##_callback, (void*)data, &zErrMsg);             \
     if (rc != SQLITE_OK) {                                                  \
       printf("%s:%d: %s\n", __FILE__, __LINE__, zErrMsg);                   \
       return rc;                                                            \
     }                                                                       \
   } while (false)
 
-#define GET_TBL3(out_name, tbl_name, col1, col2, col3)                                   \
+#define GET_TBL3(out_name, tbl_name, col1, col2, col3)                         \
   do {                                                                         \
     rc = sqlite3_exec(db,                                                      \
                       "SELECT " #col1 ", " #col2 ", " #col3 " FROM " #tbl_name \
                       " ORDER BY 1, 2, 3",                                     \
-                      gen_taco_##out_name##_callback, (void*)data, &zErrMsg); \
+                      gen_taco_##out_name##_callback, (void*)data, &zErrMsg);  \
     if (rc != SQLITE_OK) {                                                     \
       printf("%s:%d: %s\n", __FILE__, __LINE__, zErrMsg);                      \
       return rc;                                                               \
     }                                                                          \
   } while (false)
 
-#define GET_TBL4(out_name, tbl_name, col1, col2, col3, col4)                            \
+#define GET_TBL4(out_name, tbl_name, col1, col2, col3, col4)                  \
   do {                                                                        \
     rc = sqlite3_exec(db,                                                     \
                       "SELECT " #col1 ", " #col2 ", " #col3 ", " #col4        \
@@ -70,17 +70,14 @@ static int populate_taco(sqlite3* db) {
 }
 
 int main(int argc, char* argv[]) {
-  char* zErrMsg = 0;
   int rc = SQLITE_OK;
-  char* data;
 
   sqlite3_initialize();
-  //rc = sqlite3_open("./data/pldi.db", &db);
   rc = sqlite3_open(argc > 1 ? argv[1] : "./data/pldi.db", &db);
 
   if (rc) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    return (0);
+    return 1;
   } else {
     fprintf(stderr, "Opened database successfully\n");
   }
@@ -93,4 +90,3 @@ int main(int argc, char* argv[]) {
   sqlite3_close(db);
   return 0;
 }
-
