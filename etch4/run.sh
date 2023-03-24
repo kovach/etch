f346fb1c4ea05019c57309da5be67295b7daf330
Timothy Gu <timothygu99@gmail.com>
2023-03-23 21:36:16 -0700

    even sparser

--- etch4/run.sh
+++ etch4/run.sh
@@ -1,10 +1,11 @@
 mkdir -p bench-output
 
-(for size in 0.001 0.003 0.007 0.01 0.03 0.07 0.1 0.3 0.5 0.7 0.9; do
+(for size in 0.0001 0.0003 0.0007 0.001 0.003 0.007 0.01 0.03 0.07 0.1 0.3 0.5 0.7 0.9; do
 	echo data/taco-s$size.db
 done) | xargs make -j$(nproc)
 
-for size in 0.001 0.003 0.007 0.01 0.03 0.07 0.1; do
+# for size in 0.0001 0.0003 0.0007 0.001 0.003 0.007 0.01 0.03 0.07 0.1; do
+for size in 0.0001 0.0003 0.0007; do
 	echo $size
 	make run-taco-s$size >/dev/null
 
