01376afdceeffd9c5441a5219be15c5aed912541
Timothy Gu <timothygu@stanford.edu>
2023-03-23 22:15:01 -0700

    better sparse data generation

--- etch4/bench/taco-datagen.py
+++ etch4/bench/taco-datagen.py
@@ -4,44 +4,93 @@ import sys
 from pathlib import Path
 
 
-def makeV(n=100, p=0.1):
+def makeV(p=0.1, nonzeros=200):
+    # 2000 × 0.1 = 200
+
+    n = np.round(nonzeros / p)
+    matrix_size = n
+    m = np.random.rand(nonzeros)
+    m *= (matrix_size - nonzeros) / np.sum(m)
+    m += 1
+
+    # Extra correction
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+
     result = set()
-    m = np.random.rand(n)
-    it = np.nditer(m, flags=["multi_index"])
-    for i in it:
-        if i < p:
-            result.add((int(it.multi_index[0]), float(i)))
+    last = 0
+    for r in m:
+        if last >= matrix_size:
+            break
+        ri = np.round(r)
+        result.add((ri, float(r)))
+        last += ri
+    print(f"expected={nonzeros} actual={len(result)}")
     return result
 
 
-def makeA(n=100, p=0.1):
+def makeA(p=0.1, nonzeros = 400000):
+    # 2000 × 2000 × 0.1 = 400000
+
+    n = np.round(np.sqrt(nonzeros / p))
+    matrix_size = n * n
+    m = np.random.rand(nonzeros)
+    m *= (matrix_size - nonzeros) / np.sum(m)
+    m += 1
+
+    # Extra correction
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+
     result = set()
-    m = np.random.rand(n, n)
-    it = np.nditer(m, flags=["multi_index"])
-    for i in it:
-        if i < p:
-            result.add((int(it.multi_index[0]), int(it.multi_index[1]), float(i)))
+    last = 0
+    for r in m:
+        if last >= matrix_size:
+            break
+        ri = np.round(r)
+        i, j = int(ri / n), int(ri % n)
+        result.add((i, j, float(r)))
+        last += ri
+    print(f"expected={nonzeros} actual={len(result)}")
     return result
 
 
-def makeC(n=100, p=0.1):
+def makeC(p=0.1, nonzeros=800000):
+    # 200 × 200 × 200 * 0.1 = 800000
+
+    n = np.round(np.cbrt(nonzeros / p))
+    matrix_size = n * n * n
+    m = np.random.rand(nonzeros)
+    m *= (matrix_size - nonzeros) / np.sum(m)
+    m += 1
+
+    # Rounding correction
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+    m /= np.sum(np.round(m)) / matrix_size
+    # print(np.sum(np.round(m)))
+
     result = set()
-    m = np.random.rand(n, n, n)
-    it = np.nditer(m, flags=["multi_index"])
-    for i in it:
-        if i < p:
-            result.add(
-                (
-                    int(it.multi_index[0]),
-                    int(it.multi_index[1]),
-                    int(it.multi_index[2]),
-                    float(i),
-                )
-            )
+    last = 0
+    for r in m:
+        if last >= matrix_size:
+            break
+        ri = np.round(r)
+        i, j, k = int(ri / (n * n)), int(ri % (n * n) / n), int(ri % n)
+        result.add((i, j, k, float(r)))
+        last += ri
+    print(f"expected={nonzeros} actual={len(result)}")
     return result
 
 
-def main(db: Path = Path("data/pldi.db"), factor: float = 1, sparsity: float = 0.1):
+def main(db: Path = Path("data/pldi.db"), sparsity: float = 0.1):
     c = sqlite3.connect(str(db))
     c.execute("DROP TABLE IF EXISTS A")
     c.execute("DROP TABLE IF EXISTS B")
@@ -54,14 +103,15 @@ def main(db: Path = Path("data/pldi.db"), factor: float = 1, sparsity: float = 0
     )
     c.execute("CREATE TABLE V(i INTEGER NOT NULL, v REAL NOT NULL)")
     print("A")
-    c.executemany(f"INSERT INTO A VALUES(?,?,?)", makeA(int(factor * 100), sparsity))
+    # reference factor = 20
+    c.executemany(f"INSERT INTO A VALUES(?,?,?)", makeA(sparsity))
     print("B")
-    c.executemany(f"INSERT INTO B VALUES(?,?,?)", makeA(int(factor * 100), sparsity))
+    c.executemany(f"INSERT INTO B VALUES(?,?,?)", makeA(sparsity))
     print("C")
-    c.executemany(f"INSERT INTO C VALUES(?,?,?,?)", makeC(int(factor * 10), sparsity))
+    c.executemany(f"INSERT INTO C VALUES(?,?,?,?)", makeC(sparsity))
     print("V")
-    c.executemany(f"INSERT INTO V VALUES(?,?)", makeV(int(factor * 100), sparsity))
+    c.executemany(f"INSERT INTO V VALUES(?,?)", makeV(sparsity))
     c.commit()
 
 
-main(Path(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
+main(Path(sys.argv[1]), float(sys.argv[2]))
