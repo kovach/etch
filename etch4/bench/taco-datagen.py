Fix nonzeros calculation for sparsity >0.5

etch4/bench/taco-datagen.py
@@ -7,7 +7,7 @@ from pathlib import Path
 def makeV(p=0.1, nonzeros=200):
     # 2000 × 0.1 = 200
 
-    nonzeros = int(np.round(nonzeros / -np.log2(p)))
+    nonzeros = int(np.round(nonzeros / max(1, -np.log2(p))))
 
     n = int(np.round(nonzeros / p))
     matrix_size = n
@@ -36,7 +36,7 @@ def makeV(p=0.1, nonzeros=200):
 def makeA(p=0.1, nonzeros = 400000):
     # 2000 × 2000 × 0.1 = 400000
 
-    nonzeros = int(np.round(nonzeros / -np.log2(p)))
+    nonzeros = int(np.round(nonzeros / max(1, -np.log2(p))))
 
     n = int(np.round(np.sqrt(nonzeros / p)))
     matrix_size = n * n
@@ -66,7 +66,7 @@ def makeA(p=0.1, nonzeros = 400000):
 def makeC(p=0.1, nonzeros=800000):
     # 200 × 200 × 200 * 0.1 = 800000
 
-    nonzeros = int(np.round(nonzeros / -np.log2(p)))
+    nonzeros = int(np.round(nonzeros / max(1, -np.log2(p))))
 
     n = int(np.round(np.cbrt(nonzeros / p)))
     matrix_size = n * n * n