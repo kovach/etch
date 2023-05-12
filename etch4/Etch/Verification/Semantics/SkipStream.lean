33cd03f27d68a65db115e7d8704cebb17d385de8
Timothy Gu <timothygu@stanford.edu>
2023-05-11 21:08:14 -0700

    Finish SkipStream.lean

--- etch4/Etch/Verification/Semantics/SkipStream.lean
+++ etch4/Etch/Verification/Semantics/SkipStream.lean
@@ -32,7 +32,6 @@ set_option linter.uppercaseLean3 false
 open Classical
 
 noncomputable section
-
 namespace Etch.Verification
 universe u
 
@@ -65,15 +64,17 @@ theorem Stream.ext {ι α} {s₁ s₂ : Stream ι α} (h₀ : s₁.σ = s₂.σ)
     (h₂ : ∀ x y, HEq x y → (s₁.ready x ↔ s₂.ready y))
     (h₃ : ∀ x y H₁ H₂ i, HEq x y → HEq (s₁.skip x H₁ i) (s₂.skip y H₂ i))
     (h₄ : ∀ x y H₁ H₂, HEq x y → HEq (s₁.index x H₁) (s₂.index y H₂))
-    (h₅ : ∀ x y H₁ H₂, HEq x y → HEq (s₁.value x H₁) (s₂.value y H₂)) : s₁ = s₂ :=
-  by
-  cases' s₁ with σ₁ v₁ r₁ n₁ i₁ l₁; cases' s₂ with σ₂ v₂ r₂ n₂ i₂ l₂; dsimp only at *
+    (h₅ : ∀ x y H₁ H₂, HEq x y → HEq (s₁.value x H₁) (s₂.value y H₂)) : s₁ = s₂ := by
+  rcases s₁ with ⟨σ₁, v₁, r₁, n₁, i₁, l₁⟩
+  rcases s₂ with ⟨σ₂, v₂, r₂, n₂, i₂, l₂⟩
+  dsimp only at *
   subst h₀; simp only [heq_iff_eq] at *
-  obtain rfl : v₁ = v₂ := funext fun x => propext <| h₁ x x rfl;
+  obtain rfl : v₁ = v₂ := funext fun x => propext <| h₁ x x rfl
   obtain rfl : r₁ = r₂ := funext fun x => propext <| h₂ x x rfl
-  refine' ⟨rfl, rfl, rfl, _, _, _⟩ <;> try simp only [heq_iff_eq] at * <;> ext;
-  · apply h₃ x x _ _ _ rfl <;> assumption; · apply h₄ x x _ _ rfl <;> assumption
-  · apply h₅ x x _ _ rfl <;> assumption
+  congr
+  · ext; apply h₃ _ _ _ _ _ rfl
+  · ext; apply h₄ _ _ _ _ rfl
+  · ext; apply h₅ _ _ _ _ rfl
 #align Stream.ext Etch.Verification.Stream.ext
 
 section StreamDefs
@@ -145,10 +146,9 @@ theorem coeLex_lt_iff [Preorder ι] {x y : StreamOrder ι} : coeLex x < coeLex y
 #align coe_lex_lt_iff Etch.Verification.coeLex_lt_iff
 
 theorem coeLex_injective : Function.Injective (@coeLex ι)
-  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => by {
+  | ⟨x₁, y₁⟩, ⟨x₂, y₂⟩ => by
     rw [Prod.mk.injEq .., Prod.mk.injEq ..]
     simp
-  }
 #align coe_lex_injective Etch.Verification.coeLex_injective
 
 @[simp]
@@ -206,7 +206,8 @@ theorem Stream.toOrder'_val {s : Stream ι α} {q : s.σ} (hq : s.valid q) :
 
 @[simp]
 theorem Stream.coeLex_toOrder {s : Stream ι α} {q : s.σ} (hq : s.valid q) :
-    coeLex (s.toOrder q hq) = s.toOrder' q := by simp [Stream.toOrder'_val, hq]
+    coeLex (s.toOrder q hq) = s.toOrder' q := by
+  simp [coeLex, Stream.toOrder'_val, hq]
 #align Stream.coe_lex_to_order Etch.Verification.Stream.coeLex_toOrder
 
 @[simp]
@@ -225,7 +226,7 @@ theorem Stream.index'_lt_top_iff [Preorder ι] {s : Stream ι α} {q : s.σ} :
 theorem Stream.get_index' [PartialOrder ι] {s : Stream ι α} {x : s.σ} (h : (s.index' x).isSome) :
     (s.index' x).get h = s.index x (by simpa using h) := by
   generalize_proofs hq
-  simp [hq]
+  simp_rw [Stream.index', hq, Option.get]
 #align Stream.get_index' Etch.Verification.Stream.get_index'
 
 -- We use this notation so that we can explicitly ask Lean to use lexicographic comparison (rather than pointwise comparison)
@@ -251,7 +252,7 @@ variable [LinearOrder ι]
 
 /-- Extract the well-founded relation on a bounded stream -/
 def Stream.WfRel (s : Stream ι α) [IsBounded s] : s.σ → s.σ → Prop :=
-  ‹IsBounded s›.out.some
+  ‹IsBounded s›.out.choose
 #align Stream.wf_rel Etch.Verification.Stream.WfRel
 
 -- mathport name: «expr ≺ »
@@ -260,6 +261,10 @@ scoped[Streams] notation:50 a " ≺ " b => Etch.Verification.Stream.WfRel _ a b
 theorem Stream.wf (s : Stream ι α) [IsBounded s] : WellFounded s.WfRel :=
   ‹IsBounded s›.out.choose_spec.1
 #align Stream.wf Etch.Verification.Stream.wf
+
+/-- Extract the well-founded relation on a bounded stream -/
+def Stream.wfRel (s : Stream ι α) [IsBounded s] : WellFoundedRelation s.σ :=
+  ⟨s.WfRel, s.wf⟩
 
 theorem Stream.wf_valid (s : Stream ι α) [IsBounded s] :
     ∀ q hq i, (s.skip q hq i ≺ q) ∨ (i <ₗ s.toOrder q hq) ∧ s.skip q hq i = q :=
@@ -295,7 +300,7 @@ theorem Stream.wf_valid' (s : Stream ι α) [IsBounded s] (q i) :
     · rw [Prod.Lex.lt_iff']
       left
       simpa [hq] using WithTop.coe_lt_top _
-    simp [hq]
+    · simp [hq]
 #align Stream.wf_valid' Etch.Verification.Stream.wf_valid'
 
 theorem Stream.progress (s : Stream ι α) [IsBounded s] {q hq i} (h : s.toOrder q hq ≤ i) :
@@ -303,10 +308,9 @@ theorem Stream.progress (s : Stream ι α) [IsBounded s] {q hq i} (h : s.toOrder
   (s.wf_valid q hq i).resolve_right fun H => absurd H.1 h.not_lt
 #align Stream.progress Etch.Verification.Stream.progress
 
-theorem Stream.next_wf (s : Stream ι α) [IsBounded s] (q) (hq : s.valid q) : s.next q ≺ q :=
-  by
+theorem Stream.next_wf (s : Stream ι α) [IsBounded s] (q) (hq : s.valid q) : s.next q ≺ q := by
   rw [Stream.next_val hq]
-  refine' s.progress (le_of_eq _)
+  refine s.progress (le_of_eq ?_)
   simp [Stream.toOrder, hq]
 #align Stream.next_wf Etch.Verification.Stream.next_wf
 
@@ -336,20 +340,20 @@ theorem Stream.eval_valid [AddZeroClass α] (s : Stream ι α) [IsBounded s] (q 
 #align Stream.eval_valid Etch.Verification.Stream.eval_valid
 
 theorem Stream.eval₀_support [Zero α] (s : Stream ι α) (x : s.σ) (h : s.valid x) :
-    (s.eval₀ x h).support ⊆ {s.index x h} :=
-  by
+    (s.eval₀ x h).support ⊆ {s.index x h} := by
   rw [Stream.eval₀]
   split_ifs
   · exact Finsupp.support_single_subset
-  simp
+  · simp
 #align Stream.eval₀_support Etch.Verification.Stream.eval₀_support
 
 theorem Stream.eval₀_support' [Zero α] (s : Stream ι α) {x : s.σ} (h₁ : s.valid x) {i : ι}
-    (h₂ : s.eval₀ x h₁ i ≠ 0) : s.toOrder x h₁ = (i, true) :=
-  by
-  obtain rfl := Finset.eq_of_mem_singleton (s.eval₀_support x h₁ (finsupp.mem_support_iff.mpr h₂))
-  rw [Stream.eval₀] at h₂; split_ifs  at h₂ with hr
-  · simpa [Stream.toOrder, Stream.index'_val h₁] ; · simpa using h₂
+    (h₂ : s.eval₀ x h₁ i ≠ 0) : s.toOrder x h₁ = (i, true) := by
+  obtain rfl := Finset.eq_of_mem_singleton (s.eval₀_support x h₁ (Finsupp.mem_support_iff.mpr h₂))
+  rw [Stream.eval₀] at h₂
+  split_ifs at h₂ with hr
+  · simp [Stream.toOrder, Stream.index'_val h₁, hr]
+  · simp at h₂
 #align Stream.eval₀_support' Etch.Verification.Stream.eval₀_support'
 
 section Mono
@@ -366,57 +370,50 @@ def Stream.IsStrictMono (s : Stream ι α) : Prop :=
 #align Stream.is_strict_mono Etch.Verification.Stream.IsStrictMono
 
 theorem Stream.IsMonotonic.skip' {s : Stream ι α} (hs : s.IsMonotonic) (q i) :
-    s.index' q ≤ s.index' (s.skip' q i) :=
-  by
+    s.index' q ≤ s.index' (s.skip' q i) := by
   by_cases hq : s.valid q
   · rw [Stream.skip'_val hq]
     apply hs
-  rw [Stream.skip'_invalid hq]
-  exact rfl.le
+  · rw [Stream.skip'_invalid hq]
 #align Stream.is_monotonic.skip' Etch.Verification.Stream.IsMonotonic.skip'
 
 theorem Stream.isMonotonic_iff {s : Stream ι α} :
-    s.IsMonotonic ↔ ∀ q hq i hq', s.index q hq ≤ s.index (s.skip q hq i) hq' :=
-  by
-  constructor;
+    s.IsMonotonic ↔ ∀ q hq i hq', s.index q hq ≤ s.index (s.skip q hq i) hq' := by
+  constructor
   · intro h q hq i hq'
     specialize h q hq i
-    simp only [Stream.index'_val, hq, hq', WithTop.coe_le_coe] at h
-    exact h
-  intro h q hq i
-  by_cases hq' : s.valid (s.skip q hq i)
-  · simp only [Stream.index'_val, hq, hq', WithTop.coe_le_coe]
-    apply h
-  · rw [Stream.index'_invalid hq']
-    exact le_top
+    simpa [hq, hq'] using h
+  · intro h q hq i
+    by_cases hq' : s.valid (s.skip q hq i)
+    · simpa [hq, hq'] using h ..
+    · rw [Stream.index'_invalid hq']
+      exact le_top
 #align Stream.is_monotonic_iff Etch.Verification.Stream.isMonotonic_iff
 
 theorem Stream.IsMonotonic.index_le_index_next {s : Stream ι α} (h : s.IsMonotonic) (q : s.σ) :
-    s.index' q ≤ s.index' (s.next q) :=
-  by
+    s.index' q ≤ s.index' (s.next q) := by
   by_cases H : s.valid q; swap; · simp [H]
   simp only [Stream.next, H, dif_pos]
   apply h
 #align Stream.is_monotonic.index_le_index_next Etch.Verification.Stream.IsMonotonic.index_le_index_next
 
 theorem Stream.IsMonotonic.index_le_of_mem_support [AddZeroClass α] {s : Stream ι α} [IsBounded s]
-    (hs : s.IsMonotonic) {q : s.σ} : ∀ i : ι, s.eval q i ≠ 0 → s.index' q ≤ i :=
-  by
-  simp only [← Finsupp.mem_support_iff]; refine' WellFounded.induction s.wf q _
+    (hs : s.IsMonotonic) {q : s.σ} : ∀ i : ι, s.eval q i ≠ 0 → s.index' q ≤ i := by
+  simp only [← Finsupp.mem_support_iff]
+  apply s.wf.induction q
   intro q ih i hi
-  by_cases H : s.valid q; swap;
+  by_cases H : s.valid q; swap
   · exfalso
-    simpa [H] using hi
-  rw [s.eval_valid _ H] at hi
-  cases' finset.mem_union.mp (Finsupp.support_add hi) with hi hi
-  · rw [finset.mem_singleton.mp (s.eval₀_support _ H hi)]
-    exact le_of_eq (Stream.index'_val _)
-  exact trans (hs.index_le_index_next q) (ih (s.next q) (s.next_wf _ H) i hi)
+    simp [H] at hi
+  · rw [s.eval_valid _ H] at hi
+    rcases Finset.mem_union.mp (Finsupp.support_add hi) with hi | hi
+    · rw [Finset.mem_singleton.mp (s.eval₀_support _ H hi)]
+      exact le_of_eq (Stream.index'_val _)
+    · exact _root_.trans (hs.index_le_index_next q) (ih (s.next q) (s.next_wf _ H) i hi)
 #align Stream.is_monotonic.index_le_of_mem_support Etch.Verification.Stream.IsMonotonic.index_le_of_mem_support
 
 theorem Stream.IsMonotonic.eq_zero_of_lt_index [AddZeroClass α] {s : Stream ι α} [IsBounded s]
-    (hs : s.IsMonotonic) {q : s.σ} (i : ι) : ↑i < s.index' q → s.eval q i = 0 :=
-  by
+    (hs : s.IsMonotonic) {q : s.σ} (i : ι) : ↑i < s.index' q → s.eval q i = 0 := by
   contrapose!
   exact hs.index_le_of_mem_support i
 #align Stream.is_monotonic.eq_zero_of_lt_index Etch.Verification.Stream.IsMonotonic.eq_zero_of_lt_index
@@ -427,8 +424,7 @@ theorem Stream.IsStrictMono.lt {s : Stream ι α} (hs : s.IsStrictMono) (q hq i)
 #align Stream.is_strict_mono.lt Etch.Verification.Stream.IsStrictMono.lt
 
 theorem Stream.IsStrictMono.next_ne {s : Stream ι α} (hs : s.IsStrictMono) {q : s.σ}
-    (hq : s.valid q) (hr : s.ready q) : s.index' q ≠ s.index' (s.next q) :=
-  by
+    (hq : s.valid q) (hr : s.ready q) : s.index' q ≠ s.index' (s.next q) := by
   rw [Stream.next_val hq]
   exact hs.2 q hq _ rfl.le hr
 #align Stream.is_strict_mono.next_ne Etch.Verification.Stream.IsStrictMono.next_ne
@@ -448,47 +444,44 @@ theorem Stream.IsStrictMono.index_le_of_mem_support [AddZeroClass α] {s : Strea
   coeLex_le_iff.mp
     (by
       rw [Stream.coeLex_toOrder]
-      refine' (hs.spec q hv hi).trans _
+      refine (hs.spec q hv hi).trans ?_
       simpa using hs.1.index_le_of_mem_support j hj)
 #align Stream.is_strict_mono.index_le_of_mem_support Etch.Verification.Stream.IsStrictMono.index_le_of_mem_support
 
 theorem Stream.IsStrictMono.eq_zero_of_lt_index [AddZeroClass α] {s : Stream ι α} [IsBounded s]
     (hs : s.IsStrictMono) {q : s.σ} (hv) {i} (hi : s.toOrder q hv ≤ i) (j : ι) :
-    ((j, false) <ₗ s.toOrder q hv) → s.eval (s.skip q hv i) j = 0 :=
-  by
-  contrapose!
-  exact hs.index_le_of_mem_support hv hi j
+    ((j, false) <ₗ s.toOrder q hv) → s.eval (s.skip q hv i) j = 0 := by
+  contrapose
+  simpa using hs.index_le_of_mem_support hv hi j
 #align Stream.is_strict_mono.eq_zero_of_lt_index Etch.Verification.Stream.IsStrictMono.eq_zero_of_lt_index
 
 theorem fst_lt_of_lt_of_lt {α : Type _} [Preorder α] :
     ∀ {x y z : α ×ₗ Bool}, x < y → y < z → x.1 < z.1
-  | x, y, ⟨z, ff⟩, h₁, h₂ => Prod.Lex.fst_lt_of_lt_of_le (h₁.trans h₂) (by simp)
-  | x, ⟨y, tt⟩, ⟨z, tt⟩, h₁, h₂ =>
+  | x, y, ⟨z, false⟩, h₁, h₂ => Prod.Lex.fst_lt_of_lt_of_le (h₁.trans h₂) (by simp)
+  | x, ⟨y, true⟩, ⟨z, true⟩, h₁, h₂ =>
     lt_of_le_of_lt (show x.1 ≤ y from Prod.Lex.fst_le_of_le h₁.le) (by simpa using h₂)
-  | x, ⟨y, ff⟩, ⟨z, tt⟩, h₁, h₂ =>
+  | x, ⟨y, false⟩, ⟨z, true⟩, h₁, h₂ =>
     lt_of_lt_of_le (show x.1 < y from Prod.Lex.fst_lt_of_lt_of_le h₁ (by simp))
       (Prod.Lex.fst_le_of_le h₂.le)
 #align fst_lt_of_lt_of_lt Etch.Verification.fst_lt_of_lt_of_lt
 
 theorem Stream.IsStrictMono.eval_skip_eq_zero [AddZeroClass α] {s : Stream ι α} [IsBounded s]
     (hs : s.IsStrictMono) {q : s.σ} (hv : s.valid q) {i : StreamOrder ι} {j : ι}
-    (h₁ : (j, false) <ₗ i) (h₂ : i ≤ₗ s.toOrder q hv) : s.eval (s.skip q hv i) j = 0 :=
-  by
-  cases' eq_or_lt_of_le h₂ with h₂ h₂
-  · refine' hs.eq_zero_of_lt_index _ h₂.symm.le _ _
+    (h₁ : (j, false) <ₗ i) (h₂ : i ≤ₗ s.toOrder q hv) : s.eval (s.skip q hv i) j = 0 := by
+  rcases eq_or_lt_of_le h₂ with h₂ | h₂
+  · refine hs.eq_zero_of_lt_index _ h₂.symm.le _ ?_
     rwa [← h₂]
   · apply hs.1.eq_zero_of_lt_index
-    refine' lt_of_lt_of_le _ (hs.1 _ _ _)
+    refine lt_of_lt_of_le ?_ (hs.1 _ _ _)
     rw [Stream.index'_val hv, WithTop.coe_lt_coe]
     exact fst_lt_of_lt_of_lt h₁ h₂
 #align Stream.is_strict_mono.eval_skip_eq_zero Etch.Verification.Stream.IsStrictMono.eval_skip_eq_zero
 
 theorem Stream.IsStrictMono.eval₀_eq_eval_filter [AddCommMonoid α] {s : Stream ι α} [IsBounded s]
     (hs : s.IsStrictMono) (q : s.σ) (hv : s.valid q) :
-    s.eval₀ q hv = (s.eval q).filter fun i => (i, false) <ₗ s.toOrder q hv :=
-  by
+    s.eval₀ q hv = (s.eval q).filter fun i => (i, false) <ₗ s.toOrder q hv := by
   rw [s.eval_valid _ hv, Finsupp.filter_add]
-  convert (add_zero _).symm
+  convert (add_zero (M := ι →₀ α) _).symm
   · rw [Finsupp.filter_eq_self_iff]
     intro i hi
     rw [s.eval₀_support' hv hi]
@@ -496,7 +489,7 @@ theorem Stream.IsStrictMono.eval₀_eq_eval_filter [AddCommMonoid α] {s : Strea
   · rw [Finsupp.filter_eq_zero_iff]
     intro i hi
     rw [Stream.next_val hv]
-    refine' hs.eq_zero_of_lt_index hv rfl.le i hi
+    exact hs.eq_zero_of_lt_index hv rfl.le i hi
 #align Stream.is_strict_mono.eval₀_eq_eval_filter Etch.Verification.Stream.IsStrictMono.eval₀_eq_eval_filter
 
 end Mono
@@ -535,44 +528,39 @@ theorem Stream.strict_mono (s : Stream ι α) [IsStrictLawful s] : s.IsStrictMon
 theorem Stream.skip_spec (s : Stream ι α) [IsLawful s] (q : s.σ) (hq : s.valid q)
     (i : StreamOrder ι) :
     ((s.eval (s.skip q hq i)).filter fun j => i ≤ₗ (j, false)) =
-      (s.eval q).filter fun j => i ≤ₗ (j, false) :=
-  by
+      (s.eval q).filter fun j => i ≤ₗ (j, false) := by
   rw [Finsupp.filter_ext_iff]
   exact IsLawful.skip_spec q hq i
 #align Stream.skip_spec Etch.Verification.Stream.skip_spec
 
 theorem Stream.skip_lt_toOrder {s : Stream ι α} [IsLawful s] {q : s.σ} {hq : s.valid q}
-    {i : StreamOrder ι} (hi : i < s.toOrder q hq) : s.eval (s.skip q hq i) = s.eval q :=
-  by
+    {i : StreamOrder ι} (hi : i < s.toOrder q hq) : s.eval (s.skip q hq i) = s.eval q := by
   ext j
-  by_cases H : s.to_order q hq ≤ (j, tt)
+  by_cases H : s.toOrder q hq ≤ (j, true)
   · rw [IsLawful.skip_spec q hq]
     simpa [Prod.Lex.lt_iff'', Prod.Lex.le_iff''] using lt_of_lt_of_le hi H
   have : ↑j < s.index' q := by
-    simpa [hq] using fst_lt_of_lt_of_lt (show (j, ff) <ₗ (j, tt) by simp) (not_le.mp H)
+    simpa [hq] using fst_lt_of_lt_of_lt (show (j, false) <ₗ (j, true) by simp) (not_le.mp H)
   rw [s.mono.eq_zero_of_lt_index j this,
     s.mono.eq_zero_of_lt_index _ (this.trans_le (s.mono q hq i))]
 #align Stream.skip_lt_to_order Etch.Verification.Stream.skip_lt_toOrder
 
 theorem Stream.skip'_spec (s : Stream ι α) [IsLawful s] (q : s.σ) (i : StreamOrder ι) (j : ι)
-    (h : i ≤ (j, false)) : s.eval (s.skip' q i) j = s.eval q j :=
-  by
+    (h : i ≤ (j, false)) : s.eval (s.skip' q i) j = s.eval q j := by
   by_cases hq : s.valid q
   · rw [Stream.skip'_val hq, IsLawful.skip_spec _ hq _ _ h]
   rw [Stream.skip'_invalid hq]
 #align Stream.skip'_spec Etch.Verification.Stream.skip'_spec
 
 theorem Stream.skip'_lt_to_order {s : Stream ι α} [IsLawful s] {q : s.σ} {i : StreamOrder ι}
-    (hi : coeLex i < s.toOrder' q) : s.eval (s.skip' q i) = s.eval q :=
-  by
+    (hi : coeLex i < s.toOrder' q) : s.eval (s.skip' q i) = s.eval q := by
   by_cases hq : s.valid q; swap; · rw [Stream.skip'_invalid hq]
   rw [← Stream.coeLex_toOrder hq, coeLex_lt_iff] at hi
   rw [Stream.skip'_val hq, Stream.skip_lt_toOrder hi]
 #align Stream.skip'_lt_to_order Etch.Verification.Stream.skip'_lt_to_order
 
 theorem Stream.eval_skip_eq_of_false (s : Stream ι α) [IsLawful s] (q : s.σ) (hq : s.valid q) :
-    s.eval (s.skip q hq (s.index q hq, false)) = s.eval q :=
-  by
+    s.eval (s.skip q hq (s.index q hq, false)) = s.eval q := by
   by_cases hr : s.ready q
   · apply Stream.skip_lt_toOrder
     simp [Stream.toOrder, hr]
@@ -585,11 +573,14 @@ section Functor
 
 variable {ι : Type} {α β γ : Type _}
 
-/-- Solve as many goals as possible by definitional simplification, use heq/eq, and `refl` -/
-unsafe def tactic.interactive.solve_refl : tactic Unit :=
-  let tactics : List (tactic Unit) := [sorry, sorry, sorry, sorry, sorry]
-  sequence (tactics.map fun t => tactic.try t) >> tactic.skip
-#align tactic.interactive.solve_refl Etch.Verification.tactic.interactive.solve_refl
+/-- Solve goal by definitional simplification, heq/eq, and `rfl` -/
+macro (name := solveRefl) "solve_refl" : tactic =>
+  `(tactic| focus
+              try dsimp
+              try (simp only [heq_iff_eq])
+              try intros
+              try subst_vars
+              try rfl)
 
 @[simps]
 def Stream.map (f : α → β) (s : Stream ι α) : Stream ι β :=
@@ -597,17 +588,12 @@ def Stream.map (f : α → β) (s : Stream ι α) : Stream ι β :=
 #align Stream.map Etch.Verification.Stream.map
 
 @[simp]
-theorem Stream.map_id (s : Stream ι α) : s.map id = s := by
-  ext <;>
-    trace
-      "./././Mathport/Syntax/Translate/Tactic/Builtin.lean:73:14: unsupported tactic `solve_refl #[]"
+theorem Stream.map_id (s : Stream ι α) : s.map id = s :=
+  by ext <;> solve_refl
 #align Stream.map_id Etch.Verification.Stream.map_id
 
 theorem Stream.map_map (g : α → β) (f : β → γ) (s : Stream ι α) : (s.map g).map f = s.map (f ∘ g) :=
-  by
-  ext <;>
-    trace
-      "./././Mathport/Syntax/Translate/Tactic/Builtin.lean:73:14: unsupported tactic `solve_refl #[]"
+  by ext <;> solve_refl
 #align Stream.map_map Etch.Verification.Stream.map_map
 
 @[simp]
@@ -622,19 +608,21 @@ theorem Stream.toOrder'_map (s : Stream ι α) (f : α → β) : (s.map f).toOrd
 
 theorem Stream.map_value' [Zero α] [Zero β] (f : α → β) (hf : f 0 = 0) (s : Stream ι α) (q : s.σ) :
     (s.map f).value' q = f (s.value' q) := by
-  simp only [Stream.value']
+  unfold Stream.value'
   split_ifs
   · rfl
-  rw [hf]
+  · contradiction
+  · contradiction
+  · rw [hf]
 #align Stream.map_value' Etch.Verification.Stream.map_value'
 
 variable [LinearOrder ι]
 
 @[simp]
-theorem map_isBounded_iff (f : α → β) (s : Stream ι α) : IsBounded (s.map f) ↔ IsBounded s := by
-  simp only [isBounded_iff]
+theorem map_IsBounded_iff (f : α → β) (s : Stream ι α) : IsBounded (s.map f) ↔ IsBounded s := by
+  simp only [IsBounded_iff]
   rfl
-#align map_is_bounded_iff Etch.Verification.map_isBounded_iff
+#align map_is_bounded_iff Etch.Verification.map_IsBounded_iff
 
 end Functor
 
