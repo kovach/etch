4da7e993f9f7f5fd3356411e5460c498bb6778e2
prakol16 <praneeth.kolichala@gmail.com>
2024-02-26 17:53:41 -0800

    Try some more lawful modifiable

--- Etch/StreamFusion/Stream.lean
+++ Etch/StreamFusion/Stream.lean
@@ -147,8 +147,11 @@ def map (f : α → β) (s : ι →ₛ α) : ι →ₛ β := {
   s with value := f ∘ s.value
 }
 
-lemma map_eq_map (f : α → β) (s : ι →ₛ α) :
+@[simp] lemma map_eq_map (f : α → β) (s : ι →ₛ α) :
   (map f s).toStream = s.toStream.map f := rfl
+
+@[simp] lemma map_q (f : α → β) (s : ι →ₛ α) :
+  (map f s).q = s.q := rfl
 
 instance : Functor (ι →ₛ .) where
   map := map
@@ -219,7 +222,7 @@ instance {α β} [ToStream α β] : ToStream  (SparseArray ι α) (ι →ₛ β)
   stream := map ToStream.stream ∘ SparseArray.linearToStream
 
 instance : ToStream  (ArraySet ι) (ι →ₛ Bool) where
-  stream := map ToStream.stream ∘ ofBoolArray
+  stream := ofBoolArray
 
 instance {α β ι} [Hashable ι] [BEq ι] [Zero α] [ToStream α β] : ToStream  (HashMap ι α) (ι → β) where
   stream h v := ToStream.stream $ (h.findD v 0)
